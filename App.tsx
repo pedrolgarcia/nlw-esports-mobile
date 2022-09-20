78b4ca287133e0cd8ec96854c3ebf8035ccadd9d
Pedro Garcia <pedrolopesgrc@gmail.com>
2022-09-19 21:57:15 -0300

    feat: install and use fonts, with loading indicator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter"
+
 import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
   return (
     <Background>
       <StatusBar style="light" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
