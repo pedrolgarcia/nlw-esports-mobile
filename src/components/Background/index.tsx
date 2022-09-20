b6177edfbd94e0d9e596d499743ac4cd948908cc
Pedro Garcia <pedrolopesgrc@gmail.com>
2022-09-19 23:05:48 -0300

    feat: create list of games, finish home page

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ImageBackground } from 'react-native';
 
 import { styles } from './styles';
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,32 @@
-import { View } from 'react-native';
+import { FlatList, Image, View } from 'react-native';
 
 import { styles } from './styles';
+
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+import logoImg from "../../assets/logo-nlw-esports.png"
+
+import { GAMES } from "../../utils/games"
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
 
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={GAMES}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
     </View>
   );
 }
